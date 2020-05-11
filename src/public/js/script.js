58e03b4bf66ce7db3f8004b64292fc45bcb43088
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 15:41:47 +0200

    Add localstorage

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -5,43 +5,102 @@ const header = document.querySelector('main')
 const main = document.querySelector('main')
 
 fade()
-function fade(){
+
+function fade() {
     body.classList.remove('no-js-body')
     main.classList.remove('no-js-container')
     main.classList.add('fadeIn')
 }
-setTimeout(()=> {
+setTimeout(() => {
     body.classList.add('bc')
 
 }, 200)
-setTimeout(()=>{
+setTimeout(() => {
     main.classList.remove('fadeIn')
     main.classList.add('show')
-    
+
 }, 600)
 
 if (form) {
     const formbuttons = document.querySelectorAll('.poll-btn')
     console.log(formbuttons)
     formbuttons.forEach(item => {
-        item.addEventListener('click', function(){
+        item.addEventListener('click', function () {
             console.log('1')
             let attr = item.getAttribute('formaction')
             form.setAttribute('action', attr)
             form.addEventListener('submit', animatePage)
         })
     })
-    if(formbuttons.length == 0){
+    if (formbuttons.length == 0) {
         form.addEventListener('submit', animatePage)
     }
-} 
-if(buttons){
+}
+if (buttons) {
     buttons.forEach(item => {
-        item.addEventListener('click', function(e) {
+        item.addEventListener('click', function (e) {
             animateButton(e, item)
-        })    
+        })
     })
-        
+
+}
+const pollCode = document.querySelector('#pollCode')
+if(window.localStorage) {
+    let items = getItems()
+    let madePolls = document.querySelector(".pollsMade");
+    let container = document.querySelector(".pollsMade > div");
+    let header = document.querySelector(".pollsMade > div > h2")
+    console.log(items)
+    if(items == 0){
+        if(madePolls){
+            madePolls.remove()
+
+        }
+    } else {
+        let markup = ``
+
+      items.forEach((item) => {
+        markup = `
+        <div class="button">
+        <div class="translate"></div>
+        <a href="/result/${item}">${item}</a>
+      </div>`
+      container.insertAdjacentHTML("beforeend", markup);
+      })
+    }
+}
+
+if(pollCode){
+let code = pollCode.textContent
+code = code.replace(/(\r\n|\n|\r)/gm, "")
+code = Number(code)
+setItems(code)
+}
+function getItems() {
+    if (window.localStorage.length == 0) {
+        return window.localStorage.length
+    } else if (window.localStorage.polls.length >= 1) {
+        let parsed = JSON.parse(window.localStorage.getItem("polls"));
+        return parsed;
+    }
+}
+
+function setItems(data) {
+    console.log(data)
+    let items = getItems()
+    let arr = []
+    if(items.length > 1){
+        items.forEach((item) => {
+            arr.push(item)
+        })
+    } else if(items.length = 1){
+        arr.push(items)
+    }
+    
+   
+    arr.push(data)
+
+    return window.localStorage.setItem("polls", JSON.stringify(arr))
 }
 
 
@@ -50,7 +109,8 @@ function animatePage(e) {
     header.classList.add('fadeOut')
     main.classList.add('fadeOut')
     body.classList.remove('bc')
-    function nextPage(){
+    	
+    function nextPage() {
         console.log('2')
         form.submit()
     }
